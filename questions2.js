85f7eb0c1678476aaa313d9cf2e3c40648bbd168
shubhu <93861282+shubhu121@users.noreply.github.com>
2023-08-21 14:39:28 +0530

    Add files via upload

---
+++ questions2.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What data structure is used for depth first traversal of a graph?",
+    answer: "stack",
+    options: [
+      "queue",
+      "stack",
+      "list",
+      "none of the above"
+    ]
+  },
+  {
+    numb: 2,
+    question: "A pivot element to partition unsorted list is used in",
+    answer: "QuickSort",
+    options: [
+      "MergeSort",
+      "BubbleSort",
+      "QuickSort",
+      "InsertionSort"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following is a linear data structure?",
+    answer: "Array",
+    options: [
+      "Array",
+      "AVL Trees",
+      "Binary Trees",
+      "Graph"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following is not the type of queue?",
+    answer: "Single-ended Queue",
+    options: [
+      "Priority Queue",
+      "Single-ended Queue",
+      "Circular Queue",
+      "Ordinary Queue"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How are String represented in memory in C?",
+    answer: "An array of characters",
+    options: [
+      "An array of characters",
+      "The object of some class",
+      "Same as other primitive data types",
+      "LinkedList of Characters"
+    ]
+  },
+];
